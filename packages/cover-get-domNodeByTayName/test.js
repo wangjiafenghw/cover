300ac7a80c4aa6060240cd2ac0309555687b2839
wangjiafenghw <wang_jiafeng@yahoo.com>
2018-10-28 15:45:49 +0800

    测试失败了

--- packages/cover-get-domNodeByTayName/test.js
+++ packages/cover-get-domNodeByTayName/test.js
@@ -1,4 +1,5 @@
-const gdnbt = require("./index")
+const gdnbt = require("./index").cgdj
+const gdnbtSync = require("./index").cgdjSync
 
 let htmlStr = `
 <!DOCTYPE html>
@@ -15,6 +16,43 @@ let htmlStr = `
 </html>
 `;
 gdnbt(htmlStr, "h1", (res)=>{
-    console.log("h1", res)
+    console.log("Data======>\n", res)
+})
+let c = gdnbtSync(htmlStr, "body")
+console.log("syncData===========>\n", c)
+
+var fs = require('fs');
+
+function readDirRecur(folder, callback) {
+    fs.readdir(folder, function (err, files) {
+        var count = 0
+        var checkEnd = function () {
+            ++count == files.length & ; & ;
+            callback()
+        }
+        files.forEach(function (file) {
+            var fullPath = folder + '/' + file;
+            fs.stat(fullPath, function (err, stats) {
+                if (stats.isDirectory()) {
+                    return readDirRecur(fullPath, checkEnd);
+                } else {
+                    /*not use ignore files*/
+                    if (file[0] == '.') {} else {
+                        fileList.push(fullPath)
+                    }
+                    checkEnd()
+                }
+            })
+        })
+        //为空时直接回调 
+        files.length === 0 & ; & ;
+        callback()
+    })
+}
+var fileList = []
+var timeStart = new Date()
+readDirRecur('D:/github/oncedoc', function (filePath) {
+    console.log('done', new Date() - timeStart);
+    console.log(fileList);
 })
 
