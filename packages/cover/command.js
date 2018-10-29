72c36e67298a9f867fea64279e173946c4f3a4e3
wangjiafenghw <wang_jiafeng@yahoo.com>
2018-10-29 18:46:14 +0800

    交互获取配置信息

--- packages/cover/command.js
+++ packages/cover/command.js
@@ -4,20 +4,31 @@ const program = require("commander")
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 const ora = require('ora');
+const path = require('path')
+let rootName = path.basename(process.cwd())  // * 默认项目名为根目录
 
-const app = {};
+const app = {};  // * 命令相应操作方法对象
+const c_opt = {}; // * 建立项目用户交互结果对象
 /**
  * ? 逻辑逻辑！！！
  * * init操作方法
  */
 app.init = (name)=>{
-    if(name){
-        inquirer.prompt([{type: 'confirm', name: 'OK', message: `use name ${name}?`, default: true}]).then(answers => {
-            console.log(answers);
+    rootName = name?name:rootName;
+    inquirer.prompt([{type: 'string', name: 'name', message: `use name "${rootName}"?`, default: `${rootName}`}])
+    .then(answers => {// todo 项目名
+        if(typeof answers.name==="boolean") c_opt.name = pathName;
+        else                                c_opt.name = answers.name
+        inquirer.prompt([{type: 'list', name: 'input_format', message: `源码是在那个平台运行的?`, choices: ["h5", "微信小程序", "vue", "react"],default: 0}])
+        .then(answers => {// todo 入端格式
+            c_opt.input_format = answers.input_format;
+            inquirer.prompt([{type: 'checkbox', name: 'output_format', message: `输出哪些平台的工程?`, choices: ["h5", "微信小程序", "vue", "react"]}])
+            .then(answers => {// todo 输出端格式
+                c_opt.output_format = answers.output_format;
+                console.log(c_opt)
+            });
         });
-    }else{
-
-    }
+    });
 }
 
 
