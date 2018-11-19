822ad0d8c56d06e9b044fb07776bbe6b2a2ccc31
wangjiafenghw <wang_jiafeng@yahoo.com>
2018-11-19 12:35:31 +0800

    加入标签类型type值

--- packages/wx-get-wxml/wx_tag.config.js
+++ packages/wx-get-wxml/wx_tag.config.js
@@ -1,33 +1,40 @@
 /**
  * * 微信小程序wxml标签和html5对照obj
+ * @param {number} type 0>空标签 1>自闭合标签 2>普通标签
  */
 module.exports = { 
     'div': {
-        tagName: "view"
+        tagName: "view",
+        type: 2
     },
     'h1': {
         tagName: "text",
-
+        type: 2
     },
     'h2': {
         tagName: "text",
-
+        type: 2
     },
     'p': {
-        tagName: "text"
+        tagName: "text",
+        type: 2
     },
     'span': {
-        tagName: "test"
+        tagName: "test",
+        type: 2
     },
     'script': {
-        tagName: "view"
+        tagName: "view",
+        type: 0
     },
     'a': {
-        tagName: "navigator"
+        tagName: "navigator",
+        type: 2
     },
     'img': {
         tagName: "image",
-        src: "url"
+        src: "url",
+        type: 1
     }
 
 }
